1c229e41ba0c3bd4d2f2ce5cd90e4f1a841d1f9c
grep <grepfox@tutamail.com>
2026-03-14 10:25:40 +0700

    vayu: Update Graphics blobs from  LA.UM.9.1.r1-11500.02-SMxxx0.QSSI12.0
    * similar to the tag xiaomi uses on nabu without there changes.
    
    Change-Id: I352c87f40277e80a1e84388e997921c79ce84b31
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- vayu-vendor.mk
+++ vayu-vendor.mk
@@ -372,6 +372,8 @@ PRODUCT_PACKAGES += \
     libC2D2 \
     libCB \
     libOpenCL \
+    libVkLayer_q3dtools \
+    libadreno_app_profiles \
     libadreno_utils \
     libadsp_default_listener \
     libadsprpc \
@@ -385,6 +387,7 @@ PRODUCT_PACKAGES += \
     libdiag \
     libfastcvdsp_stub \
     libfastcvopt \
+    libgpudataproducer \
     libgsl \
     libllvm-glnext \
     libllvm-qcom \
